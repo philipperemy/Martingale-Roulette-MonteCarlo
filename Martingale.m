21793e0a868524f239b39e801fd5860d18b4e825
Philippe Rémy <philippe.remy@junior-aei.com>
2015-08-18 19:40:41 +0200

    n

--- Martingale.m
+++ Martingale.m
@@ -1,18 +1,15 @@
-function [ final_balance, mean_returns, vol_returns, hits_count ] = Martingale( suite_next_bet, takeProfit )
+function [ final_balance, mean_returns, vol_returns, hits_count ] = Martingale( suite_next_bet, takeProfit, money )
     
     if(takeProfit == 0)
        takeProfit = Inf; 
     end
 
     p = 18/37; %0.4865
-    money = 16*3; %48
-    %suite_next_bet = [2 3 6 12 24 48]; 
-    %suite_next_bet = [2 4 8 16 32 64]; 
     idx_next_bet = 1;
-    money_process = zeros(1); money_process(1) = money;
+    money_process(1) = money;
     next_bet = suite_next_bet(idx_next_bet);
     hits_count = 0;
-    while(money > 0 && money < takeProfit)
+    while((money > 0) && (money - next_bet > 0) && (money < takeProfit))
         win = binornd(1,p);
         if(win)
             money = money + next_bet;
@@ -20,8 +17,9 @@ function [ final_balance, mean_returns, vol_returns, hits_count ] = Martingale( 
         else
             money = money - next_bet;
             idx_next_bet = idx_next_bet + 1;
-
             if(idx_next_bet > length(suite_next_bet))
+                money_process(end+1) = money;
+                hits_count = hits_count + 1;
                 break; 
             end
         end
